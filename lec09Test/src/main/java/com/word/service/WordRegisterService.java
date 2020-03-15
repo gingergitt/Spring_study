cb66556489ee8bd57840daf9a512f5a8ec67455a
gingergitt <mro8822@gmail.com>
2020-03-16 00:17:48 +0900

    20200316 spring commit

---
+++ lec09Test/src/main/java/com/word/service/WordRegisterService.java
@@ -0,0 +1,45 @@
+package com.word.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.word.WordSet;
+import com.word.dao.WordDao;
+
+public class WordRegisterService {
+	
+	@Autowired
+	private WordDao wordDao;
+
+	//property나 메서드에 Autowired를 쓰려면 반드시 default 생성자가 있어야 한다 
+	public WordRegisterService() { 
+	}
+
+	@Autowired 
+	public WordRegisterService(WordDao wordDao) {
+		this.wordDao = wordDao;
+	}
+
+	// 단어등록 메서드 -> wordset에 저장시키는 것이다.
+	// wordSet객체를 가져와서 비교한다. (같은 단어가 있으면 걸러내야하니까 )
+	public void register(WordSet wordSet) {
+		String wordKey = wordSet.getWordKey();
+		if (verify(wordKey)) {
+			wordDao.insert(wordSet); // wordSet에 insert한다.
+		} else {
+
+			System.out.println("The word has already registered");
+		}
+	}
+	
+	@Autowired
+	public boolean verify(String wordKey) {
+		WordSet wordSet = wordDao.select(wordKey); // wordKey db에 넣기
+
+		return wordSet == null ? true : false; // wordSet이 null이면(등록되지않은 단어) true로 단어를 등록한다.
+	}
+
+	public void setWordDao(WordDao wordDao) {
+		this.wordDao = wordDao;
+	}
+
+}
